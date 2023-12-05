6d7613f741b2dfdf0f2b7ee155bfb3d3fc59737f
marciobonatto <marciobonattodsg@gmail.com>
2023-12-05 06:19:50 -0300

    Segunda versão do desafio foi criada

--- index.js
+++ index.js
@@ -9,24 +9,34 @@ let nivel
 console.log("=".repeat(22))
 console.log("SUA EQUIPE DE HERÓIS:")
 console.log("=".repeat(22))
+
 for (let cont = 0; cont < herois.length; cont++) {
     xp = herois[cont][1]
-    if (xp <= 1000) {
-        nivel = "Ferro"
-    } else if (xp <= 2000) {
-        nivel = "Bronze"
-    } else if (xp <= 6000) {
-        nivel = "Prata"
-    } else if (xp <= 7000) {
-        nivel = "Ouro"
-    } else if (xp <= 8000) {
-        nivel = "Platina"
-    } else if (xp <= 9000) {
-        nivel = "Ascendente"
-    } else if (xp <= 10000) {
-        nivel = "Imortal"
-    } else if (xp >= 10001) {
-        nivel = "Radiante"
+    switch (xp >= 0) {
+        case xp <= 1000:
+            nivel = "Ferro"
+            break
+        case xp <= 2000:
+            nivel = "Bronze"
+            break
+        case xp <= 6000:
+            nivel = "Prata"
+            break
+        case xp <= 7000:
+            nivel = "Ouro"
+            break
+        case xp <= 8000:
+            nivel = "Platina"
+            break
+        case xp <= 9000:
+            nivel = "Ascendente"
+            break
+        case xp <= 10000:
+            nivel = "Imortal"
+            break
+        case xp >= 10001:
+            nivel = "Radiante"
+            break
     }
     console.log(`O Herói de nome ${herois[cont][0]}, com XP de ${xp}, está no nível ${nivel}.`)
 }
